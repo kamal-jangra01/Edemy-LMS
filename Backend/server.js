6fe09c90ed912ae9d6f79f50c11a7cc95ba7abd7
kamal <kamaldeep63951@gmail.com>
2026-04-07 17:14:04 +0530

    First commit

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import clerkWebhooks from './controllers/Webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+
+//Initialize express
+const app = express()
+
+// connect to database
+await connectDB()
+
+// middlewares
+app.use(cors());
+app.use(clerkMiddleware())
+
+// Routes
+app.get('/', (req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(),clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+
+//Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);    
+})
